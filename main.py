526ad9dd7277990edede3d2683b10680288af676
karnapratyush <pratyushkumarkarna@gmail.com>
2020-09-08 02:32:02 +0530

    fea: added code for closing the window of pygame

--- main.py
+++ main.py
@@ -6,6 +6,24 @@ import pygame
 pygame.init()
 
 
-#creating a screen
+# creating a screen
 
-screen=pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((800, 600))
+
+# we can observe here that the screen disappears quickly. To change that
+# one way is
+
+# while True:
+#     pass
+
+# but this will hang the window as it is neverending loop
+
+# so we will close the windo when someone is clicking on x button
+# for that we will use the following code
+
+
+is_running = 1
+while is_running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            is_running = 0
